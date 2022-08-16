761a887651f72b45777abde7a0629b8906216a0c
Игорь <ponycss1337@mail.ru>
2022-08-17 01:03:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<style>
-	body {
-		margin: 0;
-	}
-	</style>
-</head>
-<body>
-	<script src="./app.bundle.js"></script>
-</body>
-</html>
+const GuessingGame = require('./src/guessing-game.js');
+
+window.game = new GuessingGame();
