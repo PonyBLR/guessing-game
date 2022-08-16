f790cd9dba8b52185d65281b359fb857a7700eb1
Игорь <ponycss1337@mail.ru>
2022-08-17 01:05:04 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
+    constructor() {
 
     }
 
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+        let guessNumber;
+    }
     guess() {
-
+        this.guessNumber = Math.ceil((this.max - this.min) / 2) + this.min;
+        return this.guessNumber;
     }
 
     lower() {
+        this.max = this.guessNumber;
 
     }
 
     greater() {
+        this.min = this.guessNumber;
 
     }
 }
